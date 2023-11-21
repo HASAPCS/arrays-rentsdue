cb288eb24ac6abac49f344ed21ab2e39661f9c61
rentsdue <hoganwong88@gmail.com>
2023-11-21 13:31:36 +0800

    done i guess

--- src/main/java/com/example/ArrayOperations.java
+++ src/main/java/com/example/ArrayOperations.java
@@ -3,36 +3,64 @@ package com.example;
 public class ArrayOperations {
 
     public static void main(String[] args) {
-        // Exercise 1: Initialize and Print an Array
-        // TODO: Initialize an array of integers and print its contents.
+        int[] Xi_Jinping={69, 420, 111, 556, 79294, 55503, 1, 2};
+        for (int Int: Xi_Jinping) {
+            System.out.println(Int);
+        }
     }
 
     // Exercise 2: Find the Maximum Value in an Array
     public static int findMaximumValue(int[] array) {
-        // TODO: Implement the logic to find the maximum value in the array.
-        return 0; // Placeholder return value
+        int maxValue=array[0];
+        for (int num: array) {
+            if (num>maxValue) {
+                maxValue=num;
+            }
+        }
+        return maxValue;
     }
 
     // Exercise 3: Check for a Specific Value
     public static boolean containsValue(int[] array, int value) {
-        // TODO: Implement the logic to check if the array contains the specified value.
+        for (int num: array) {
+            if (num==value) {
+                return true;
+            }
+        }
         return false; // Placeholder return value
     }
 
     // Exercise 4: Calculate the Average of Array Elements
     public static double calculateAverage(int[] array) {
-        // TODO: Implement the logic to calculate the average of the array elements.
-        return 0.0; // Placeholder return value
+        double sum=0;
+        for (int num: array) {
+            sum +=num;
+        }
+        return sum/array.length; // Placeholder return value
     }
 
     // Exercise 5: Reverse an Array
     public static int[] reverseArray(int[] array) {
-        // TODO: Implement the logic to reverse the array.
-        return new int[0]; // Placeholder return value
+        for (int i = 0; i < array.length / 2; i++) {
+            int j = array.length - 1 - i;
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
     }
+    
 
     // Exercise 6: Sort an Array
     public static void sortArray(int[] array) {
-        // TODO: Implement a sorting algorithm to sort the array.
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }      
     }
 }
